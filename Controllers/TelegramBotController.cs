162f24284240548b46f5391d07a133a295d1f95b
duzhyk <anthony.duzhik@gmail.com>
2021-09-02 00:01:14 +0300

    fix

--- Controllers/TelegramBotController.cs
+++ Controllers/TelegramBotController.cs
@@ -1,11 +1,11 @@
 using System;
 using System.IO;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Newtonsoft.Json;
 
 namespace Teledoro.Controllers
 {
@@ -21,28 +21,15 @@ namespace Teledoro.Controllers
         }
         
         [HttpPost("{token}")]
-        public async Task<ActionResult> Post(string token, [FromBody] Update update)
+        public async Task<ActionResult> Post(string token)
         {
             var client = new TelegramBotClient(token);
             
-            
-            var sr = new StreamReader(Request.Body);
-            var requestContent =  await sr.ReadToEndAsync();
+            using var sr = new StreamReader(Request.Body);
+            var bodyContent =  await sr.ReadToEndAsync();
+            var update = JsonConvert.DeserializeObject<Update>(bodyContent);
 
-            var response = await client.SendTextMessageAsync(5646908, update.ToString());
-//
-            //var request = new HttpRequestMessage()
-            //{
-            //    Method = HttpMethod.Get,
-            //    RequestUri = new Uri("https://api.telegram.org/bot" + token + "/sendMessage"),
-            //    Content = new StringContent(@"{
-            //                                      ""chat_id"": 5646908,
-            //                                      ""text"": ""hi, there " + requestContent + @" ""
-            //                                  }", Encoding.UTF8, "application/json")
-            //};
-//
-            //var response = await client.SendAsync(request).ConfigureAwait(false);
-            //response.EnsureSuccessStatusCode();
+            var response = await client.SendTextMessageAsync(5646908, update.Message?.Text);
 
             return Ok();
         }
