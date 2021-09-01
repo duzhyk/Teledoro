4518deac2e6baed3cc3a2eba4bda6e3839770059
duzhyk <anthony.duzhik@gmail.com>
2021-09-01 18:33:40 +0300

    apply telegram.bot

--- Controllers/TelegramBotController.cs
+++ Controllers/TelegramBotController.cs
@@ -1,8 +1,11 @@
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace Teledoro.Controllers
 {
@@ -18,22 +21,28 @@ namespace Teledoro.Controllers
         }
         
         [HttpPost("{token}")]
-        public async Task<ActionResult> Post(string token)
+        public async Task<ActionResult> Post(string token, Update update)
         {
-            var request = new HttpRequestMessage()
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://api.telegram.org/bot" + token + "/sendMessage"),
-                Content = new StringContent(@"{
-                                                  ""chat_id"": 5646908,
-                                                  ""text"": ""hi, there""
-                                              }", Encoding.UTF8, "application/json")
-            };
+            var client = new TelegramBotClient(token);
+            var response = await client.SendTextMessageAsync(5646908, "msg from tg bot client");
+            
+            //var sr = new StreamReader(Request.Body);
+            //var requestContent =  await sr.ReadToEndAsync();
+//
+            //var request = new HttpRequestMessage()
+            //{
+            //    Method = HttpMethod.Get,
+            //    RequestUri = new Uri("https://api.telegram.org/bot" + token + "/sendMessage"),
+            //    Content = new StringContent(@"{
+            //                                      ""chat_id"": 5646908,
+            //                                      ""text"": ""hi, there " + requestContent + @" ""
+            //                                  }", Encoding.UTF8, "application/json")
+            //};
+//
+            //var response = await client.SendAsync(request).ConfigureAwait(false);
+            //response.EnsureSuccessStatusCode();
 
-            var response = await client.SendAsync(request).ConfigureAwait(false);
-            response.EnsureSuccessStatusCode();
-
-            return Ok(response);
+            return Ok(update);
         }
     }
 }
